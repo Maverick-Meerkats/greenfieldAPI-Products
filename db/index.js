41cf411cf9adb19559ee0d5e2a778c77a647b723
Richard Horvath <richard.horvath001@gmail.com>
2019-11-07 20:02:10 -0500

    update not much success

--- db/index.js
+++ db/index.js
@@ -1,5 +1,8 @@
 const { Pool, Client } = require("pg");
 const Sequelize = require("sequelize");
+const csv = require("csv-parser");
+const fs = require("fs");
+const readline = require("readline");
 const sequelize = new Sequelize("greenFieldProducts", "postgres", "babaka11", {
   host: "localhost",
   dialect: "postgres",
@@ -13,6 +16,120 @@ sequelize
   .catch(err => {
     console.error("Unable to connect to the database:", err);
   });
+const Photos = sequelize.define(
+  "photos",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
+    // attributes
+    styleId: {
+      type: Sequelize.INTEGER
+    },
+    url: {
+      type: Sequelize.TEXT
+    },
+    thumbnail_url: {
+      type: Sequelize.TEXT
+    }
+  },
+  {
+    // options
+  }
+);
+Photos.sync({ force: true }).then(() => {
+  let results = [];
+  let badData = [];
+  let files = fs.readdirSync("./db/photo_split");
+
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(`./db/photo_split/${files[0]}`),
+    console: false
+  });
+  readInterface.on("line", function(line) {
+    let lineArr = line.split(",");
+    const obj = {};
+    lineArr[0] ? (obj.id = lineArr[0].trim()) : 0;
+    lineArr[1] ? (obj.styleId = lineArr[1].trim()) : 9999;
+    lineArr[2] ? (obj.url = lineArr[2].trim().toString()) : "habg";
+    lineArr[3] ? (obj.thumbnail_url = lineArr[3].trim().toString()) : "jabg";
+    if (
+      typeof Number(obj.id) === "number" &&
+      typeof Number(obj.styleId) === "number" &&
+      obj.url &&
+      obj.thumbnail_url
+    ) {
+      Photos.create(obj);
+    }
+  });
+});
+
+// sequelize.query(
+//   `DROP TABLE IF EXISTS productInfo;
+//     DROP TABLE IF EXISTS features;
+//     DROP TABLE IF EXISTS related;
+//     DROP TABLE IF EXISTS skus;
+//     DROP TABLE IF EXISTS styles;
+
+//   CREATE TABLE productInfo(
+//             id INT PRIMARY KEY,
+//             name VARCHAR(100),
+//             slogan character varying,
+//             description TEXT,
+//             category VARCHAR(255),
+//             default_price VARCHAR(50)
+//             );
+//    CREATE TABLE features (
+//             id INT PRIMARY KEY,
+//             product_id INT,
+//             feature VARCHAR(55),
+//             value VARCHAR(55)
+//             );
+//     CREATE TABLE related (
+//             id INT PRIMARY KEY,
+//             current_product_id INT,
+//             related_product_id INT
+//             );
+//     CREATE TABLE skus (
+//             id INT PRIMARY KEY,
+//             styleId INT,
+//             size VARCHAR(20),
+//             quantity INT
+//              );
+//     CREATE TABLE styles (
+//         id INT PRIMARY KEY,
+//         productId INT,
+//         name VARCHAR(60),
+//         sale_price VARCHAR(5),
+//         original_price INT,
+//         default_style SMALLINT
+//     );
+
+//           COPY productInfo(id,name,slogan,description,category, default_price)
+//           FROM 'C:\\DatabaseData\\product.csv' DELIMITER ',' CSV HEADER;
+
+//           COPY features(id,product_id,feature,value)
+//           FROM 'C:\\DatabaseData\\features.csv' DELIMITER ',' CSV HEADER;
+
+//           COPY related(id,current_product_id,related_product_id)
+//           FROM 'C:\\DatabaseData\\related.csv' DELIMITER ',' CSV HEADER;
+
+//           COPY skus(id, styleId,size, quantity)
+//           FROM 'C:\\DatabaseData\\skus.csv' DELIMITER ',' CSV HEADER;
+
+//           UPDATE productInfo SET default_price=trim(default_price);
+//           UPDATE productInfo SET default_price=trim(leading 'default_price: ' from default_price);
+//           COPY styles (id,productId,name,sale_price,original_price,default_style)
+//           FROM 'C:\\DatabaseData\\styles.csv' DELIMITER ',' CSV HEADER;`
+// );
+
+//   .then((err, res) => {
+//     console.log(err, res);
+//     sequelize
+//       .query(`SELECT * FROM photos where id = 694;`)
+//       .then((err, res) => console.log(err, res));
+//   });
 
 // sequelize.close();
 // const pool = new Pool({
@@ -30,40 +147,10 @@ sequelize
 // sequelize.query("DROP TABLE IF EXISTS productInfo;").then((err, res) => {
 //   console.log(err, res);
 // });
-// sequelize
-//   .query(
-//     `DROP TABLE IF EXISTS productInfo;
-//   CREATE TABLE productInfo(
-//             id INT PRIMARY KEY,
-//             name VARCHAR(100),
-//             slogan character varying,
-//             description TEXT,
-//             category VARCHAR(255),
-//             default_price VARCHAR(50)
-//             );
-//    CREATE TABLE features (
-//             id INT PRIMARY KEY,
-//             product_id INT,
-//             feature VARCHAR(55),
-//             value VARCHAR(55)
-//             );
-//             COPY productInfo(id,name,slogan,description,category, default_price)
-//             FROM 'C:\\DatabaseData\\product.csv' DELIMITER ',' CSV HEADER;
-//             COPY features(id,product_id,feature,value)
-//             FROM 'C:\\DatabaseData\\features.csv' DELIMITER ',' CSV HEADER;
-//             UPDATE productInfo SET default_price=trim(default_price);
-//             UPDATE productInfo SET default_price=trim(leading 'default_price: ' from default_price);`
-//   )
-//   .then((err, res) => {
-//     console.log(err, res);
-//     sequelize
-//       .query(`SELECT * FROM productInfo where id = 11;`)
-//       .then((err, res) => console.log(err, res));
-//   });
-sequelize
-  .query(`SELECT * FROM features where id = 11;`)
-  .then((err, res) => console.log(err, res));
 
+// sequelize
+//   .query(`SELECT * FROM related where id = 11;`)
+//   .then((err, res) => console.log(err, res));
 // pool.query(
 //   `COPY productInfo(id,name,description,category,price)
 // FROM 'C:\DatabaseData\product.csv' DELIMITER ',' CSV HEADER;`,
