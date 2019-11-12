1d28c6d9ba5865018f7748c24deb0e60a02aa13b
Richard Horvath <richard.horvath001@gmail.com>
2019-11-11 20:01:20 -0500

    merged with master

--- db/models/product.js
+++ db/models/product.js
@@ -1,11 +1,7 @@
 const sequelize = require("../index.js");
 
 module.exports = {
-<<<<<<< HEAD
   getOneProduct: id => {
-=======
-  getOne: id => {
->>>>>>> master
     return sequelize.query(
       `SELECT * FROM productInfo WHERE productInfo.id=${id}`
     );
